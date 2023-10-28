c762f28db1e1eeaa8021e7cc85c1f4fabcbe082c
moris334 <143335079+moris334@users.noreply.github.com>
2023-10-28 16:49:21 +0200

    Add files via upload

--- pitagorina-teorema.py
+++ pitagorina-teorema.py
@@ -1,4 +1,5 @@
 #Ovaj program je napravio boris smiljanić
+#kk znači kvadratni koren
 import math
 def ptrougao():
     a = int(input("Unesi stranicu a: "))
@@ -7,7 +8,11 @@ def ptrougao():
     b *= b
     c = a+b
     c_kk = math.sqrt(c)
-    print("C kvadrirano ", c, " ,C ", c_kk)
+    a_kk = math.sqrt(a)
+    b_kk = math.sqrt(b)
+    obim = a_kk + b_kk + c_kk
+    povrsina =(a_kk*b_kk)/2
+    print("C kvadrirano ", c, " ,C ", c_kk," ,obim ",obim," i povrsina ",povrsina)
 
 def pravugaonik():
     a = int(input("Unesi stranicu a: "))
@@ -16,37 +21,51 @@ def pravugaonik():
     b *= b
     c = a+b
     c_kk = math.sqrt(c)
-    print("D kvadrirano ", c, " ,D ", c_kk)
+    a_kk = math.sqrt(a)
+    b_kk = math.sqrt(b)
+    obim = (a_kk*2)+(b_kk*2)
+    povrsina = a_kk*b_kk
+    print("D kvadrirano ", c, " ,D ", c_kk," ,obim ",obim," i povrsina ",povrsina)
+    
 
 def kvadrat():
     a = int(input("Unesi stranicu a: "))
     a *= a
     c = a*2
-    c_kk = math.sqrt(c)
-    print("D kvadrirano ", c, " ,D ", c_kk)
+    c_kk = math.sqrt(2*(a*a))
+    a_kk = math.sqrt(a)
+    obim = a_kk * 4
+    povrsina = a_kk * a_kk
+    print("D kvadrirano ", c, " ,D ", c_kk," ,obim ",obim," i povrsina ",povrsina)
 
 def jedkrak():
     a = int(input("Unesi stranicu a: "))
-    ha = int(input("Unesi stranicu b: "))
+    ha = int(input("Unesi visinu ha: "))
     a /= 2
     a *= a
     ha *= ha
-    c = a+b
+    c = a+ha
     c_kk = math.sqrt(c)
-    print("Ha kvadrirano ", c, " ,Ha ", c_kk)
+    a_kk = math.sqrt(a)
+    ha_kk = math.sqrt(ha)
+    obim = a_kk + (2*c_kk)
+    povrsina =(a_kk*ha_kk)/2
+    print("B kvadrirano ", c, " ,B ", c_kk," ,obim ",obim," i povrsina ",povrsina)
+while True:
+    unos = int(input("Izaberi: 0)izađi;1)pravougli trougao;2)pravugaonik;3)kvadrat;4)jednakokraki trougao; "))
 
-unos = int(input("Izaberi: 1)pravougli trougao;2)pravugaonik;3)kvadrat;4)jednakokraki trougao"))
-
-if unos == 1:
-    ptrougao()
-elif unos == 2:
-    pravugaonik()
-elif unos == 3:
-    kvadrat()
-elif unos == 4:
-    jedkrak()
-else:
-    print("Opcija nije validna")
+    if unos == 1:
+        ptrougao()
+    elif unos == 2:
+        pravugaonik()
+    elif unos == 3:
+        kvadrat()
+    elif unos == 4:
+        jedkrak()
+    elif unos == 0:
+        break
+    else:
+        print("Opcija nije validna")
     
 
 
